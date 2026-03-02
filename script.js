d71474ccb7d83951111fe00dbf8f2afd61928b37
Judy Derrick <jgd6426@rit.edu>
2026-03-02 13:55:48 -0500

    new home banner

--- script.js
+++ script.js
@@ -1,15 +1,5 @@
 // header responsiveness
 // switch links to hamburger menu on smaller screens
-/* Toggle between adding and removing the "responsive" class to topnav when the user clicks on the icon */
-
-// function toggleMenu() {
-//     var x = document.getElementById("myTopnav");
-//     if (x.className === "topnav") {
-//         x.className += " responsive";
-//     } else {
-//         x.className = "topnav";
-//     }
-// };
 
 /* Toggle between adding and removing the "responsive" class to topnav when the user clicks on the icon */
 function toggleMenu() {
@@ -20,3 +10,20 @@ function toggleMenu() {
     x.className = "topnav";
   }
 }
+
+// text slideshow for home banner (automatic)
+let slideIndex = 0;
+showSlides(slideIndex);
+
+function showSlides() {
+  let i;
+  let slides = document.getElementsByClassName("mySlides");
+  for (i = 0; i < slides.length; i++) {
+    slides[i].style.display = "none";
+  }
+  slideIndex++;
+  if (slideIndex > slides.length) {slideIndex = 1}
+  slides[slideIndex-1].style.display = "block";
+  setTimeout(showSlides, 8000); // Change text every 8 seconds
+}
+
